1ff09d02323dae726f170b66a821908d332a263d
Toucanite <lucas.dnt@eduge.ch>
2017-01-11 09:12:20 +0100

    arabe

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 	// On démarre une session
   session_start();
   $articles = array('appareil', 'mixer', 'barbecue', 'cartesd', 'jumelles', 'chocolats', 'telephones', 'lampe', 'porsche');
+	// Si l'utilisateur demande de vider le panier on détruit la session et on en recréer une
+  if (isset($_GET['detruire'])) {
+    if ($_GET['detruire']) {
+      session_destroy();
+      #exit;
+      session_start();
+    }
+  }
   $panier = isset($_SESSION['panier']) ? $_SESSION['panier'] : array();
   if (isset($_GET['article'])) {
     array_push($panier, $_GET['article']);
@@ -9,13 +17,6 @@
 
   $_SESSION['articles'] = $articles;
   $_SESSION['panier'] = $panier;
-	// Si l'utilisateur demande de vider le panier on détruit la session et on en recréer une
-  if (isset($_GET['detruire'])) {
-    if ($_GET['detruire']) {
-      session_destroy();
-      exit;
-    }
-  }
 
 	// On récupère le nombre d'objets
   $nbrObjets = count($panier);
@@ -45,7 +46,7 @@
 			<a href="index.php?article=8"><img src="porsche.jpg" alt="porsche"/></a>
 			<br/><br/>
 			<?php
-				echo '<p>Votre panier contient : <br/><br/>';
+				echo '<p>Votre panier contient : '.$nbrObjets.'<br/><br/>';
 			?>
 			<a href="panier.php">Voir mon panier</a>
 		</div>
